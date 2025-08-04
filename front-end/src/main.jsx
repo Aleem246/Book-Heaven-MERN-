16ad960dc485b20e051a158a2b77a49a529c6753
Aleem246 <21ag1a6640@gmail.com>
2025-08-04 16:23:29 +0530

    ready for production

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import App from './App'
+import App from './App.jsx'
 import {Provider} from "react-redux"
-import store from "/src/store/index"
+import store from "/src/store/index.js"
 import {BrowserRouter as Router} from "react-router-dom"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
